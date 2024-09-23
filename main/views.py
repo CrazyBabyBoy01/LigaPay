a74de977432d41e59b3c200014125181700220fa
CrazyBabyBoy01 <72690062+CrazyBabyBoy01@users.noreply.github.com>
2024-09-23 18:54:36 +0300

    23/09/24 18:54

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from django.template import context
 
 
 def index(request):
-    context = {"title": "Home", "content": "Главная страница"}
+    context = {"title": "LigaPay", "subtitle": "LigaPay - является гарантом на всех этапах сделки."}
     return render(request, "main/index.html", context)
 
 
