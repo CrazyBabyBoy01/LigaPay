d6aef646f037a3b20bc1bea9c85e054aca5d4404
CrazyBabyBoy01 <72690062+CrazyBabyBoy01@users.noreply.github.com>
2025-09-23 17:59:22 +0300

    refactoring wallet+orders

--- wallet/signals.py
+++ wallet/signals.py
@@ -1,9 +1,13 @@
+import logging
+
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Wallet
 
+
+logger = logging.getLogger(__name__)
 
 'Это сигнал нужен, для того что бы при регистрации нового пользователя,'
 'у него автоматически создавался кошелек,а то пришлось бы создавать вручную для каждого пользователя'
@@ -12,5 +16,5 @@ from .models import Wallet
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_wallet(sender, instance, created, **kwargs):
     if created:  # Если пользователь только что создан
-        Wallet.objects.create(user=instance)
-        print(f'Кошелек создан для пользователя {instance.username}')
+        Wallet.objects.get_or_create(user=instance)
+        logger.info('Кошелёк создан для пользователя %s', instance.username)
